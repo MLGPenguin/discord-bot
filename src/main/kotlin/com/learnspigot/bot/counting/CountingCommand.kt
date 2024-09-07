0be149342f93a6716329cc0800781901b8601580
MLGPenguin <baww123@gmail.com>
2024-09-08 01:31:28 +1000

    Counting Command -> Lam-

--- src/main/kotlin/com/learnspigot/bot/counting/CountingCommand.kt
+++ src/main/kotlin/com/learnspigot/bot/counting/CountingCommand.kt
@@ -1,48 +1,45 @@
 package com.learnspigot.bot.counting
 
-import com.learnspigot.bot.Bot.jda
 import com.learnspigot.bot.database.counting.CountingManager
 import com.learnspigot.bot.database.profile.getProfile
 import com.learnspigot.bot.util.InvisibleEmbed
-import dev.minn.jda.ktx.events.onCommand
-import dev.minn.jda.ktx.interactions.components.getOption
 import net.dv8tion.jda.api.entities.User
+import revxrsal.commands.annotation.Command
+import revxrsal.commands.annotation.Optional
+import revxrsal.commands.jda.actor.SlashCommandActor
 
-object CountingCommand {
+class CountingCommand {
 
-    init {
-        jda.onCommand("countingstats") { event ->
-            val user = event.getOption<User>("user")
-
-            if (user == null) { // Server Stats
-                event.replyEmbeds(InvisibleEmbed {
-                    title = "Server counting statistics"
-                    description = """
+    @Command("countingstats")
+    fun onCountingStatsCommand(actor: SlashCommandActor, @Optional target: User? = null) {
+        if (target == null) { // Server Stats
+            actor.commandEvent().replyEmbeds(InvisibleEmbed {
+                title = "Server counting statistics"
+                description = """
                         - Last Count: ${CountingManager.currentCount}
                         - Total Counts: ${CountingManager.serverTotalCounts}
                         - Highest Count: ${CountingManager.topServerCount}
                     """.trimIndent()
 
-                    field {
-                        name = "Top 5 counters"
-                        value = CountingManager.getTop5().joinToString("") { profile ->
-                            "\n- <@${profile.id}>: ${profile.totalCounts}"
-                        }
+                field {
+                    name = "Top 5 counters"
+                    value = CountingManager.getTop5().joinToString("") { profile ->
+                        "\n- <@${profile.id}>: ${profile.totalCounts}"
                     }
+                }
 
-                }).setEphemeral(true).queue()
-            } else {
-                val profile = user.getProfile()
+            }).setEphemeral(true).queue()
+        } else {
+            val profile = target.getProfile()
 
-                event.replyEmbeds(InvisibleEmbed {
-                    title = "${user.name}'s counting statistics"
-                    description = """
+            actor.commandEvent().replyEmbeds(InvisibleEmbed {
+                title = "${target.name}'s counting statistics"
+                description = """
                         - Total Counts: ${profile?.totalCounts}
                         - Highest Count: ${profile?.highestCount}
                         - Mistakes: ${profile?.countingFuckUps}
                     """.trimIndent()
-                })
-            }
+            })
         }
     }
 }
